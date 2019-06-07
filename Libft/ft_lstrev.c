a6d511a64f4054d250b21150640fd766b3257d18
winorth <winorth@student.42.us.org>
2019-06-07 13:16:50 -0700

    added lstcount & lstrev

---
+++ Libft/ft_lstrev.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstrev.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: winorth <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/31 15:25:57 by winorth           #+#    #+#             */
+/*   Updated: 2019/05/31 15:25:59 by winorth          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstrev(t_list **alst)
+{
+	t_list *prev;
+	t_list *curr;
+	t_list *next;
+
+	prev = NULL;
+	curr = *alst;
+	while (curr != NULL)
+	{
+		next = curr->next;
+		curr->next = prev;
+		prev = curr;
+		curr = next;
+	}
+	*alst = prev;
+}
